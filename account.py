972ccc4056932379a4853362f5af303443777495
enzolda <enzolealdearagao@gmail.com>
2024-07-30 23:10:35 -0300

    Modificações pedidas pelo professor foram feitas

--- account.py
+++ account.py
@@ -8,3 +8,16 @@ class Account:
         if amount > self.balance:
             raise ValueError("Insufficient funds")
         self.balance -= amount
+
+    def deposit(self, amount):
+        if amount < 0:
+            raise ValueError("Cannot deposit negative amount")
+        if amount == 0:
+            raise ValueError("Deposit amount must be greater than zero")
+        self.balance += amount
+
+    def transfer(self, receiver, amount):
+        if not (receiver):
+            raise ValueError("Receiving account doesn't exist")
+        self.withdraw(amount)
+        receiver.deposit(amount)
--- test_account.py
+++ test_account.py
@@ -3,19 +3,41 @@ from account import Account
 
 class TestAccount(unittest.TestCase):
     def setUp(self):
-        self.account = Account(initial_balance=100)
+        self.account1 = Account(initial_balance=100)
+        self.account2 = Account(initial_balance=25)
     
     def test_withdraw_success(self):
-        self.account.withdraw(50)
-        self.assertEqual(self.account.balance, 50)
+        self.account1.withdraw(50)
+        self.assertEqual(self.account1.balance, 50)
     
     def test_withdraw_insufficient_funds(self):
         with self.assertRaises(ValueError):
-            self.account.withdraw(150)
+            self.account1.withdraw(150)
     
     def test_withdraw_negative_amount(self):
         with self.assertRaises(ValueError):
-            self.account.withdraw(-10)
+            self.account1.withdraw(-10)
+
+    def test_deposit_success(self):
+       self.account1.deposit(50)
+       self.assertEqual(self.account1.balance, 150)
+
+    def test_deposit_negative_amount(self):
+        with self.assertRaises(ValueError):
+            self.account1.deposit(-20)
+
+    def test_deposit_no_amount(self):
+        with self.assertRaises(ValueError):
+            self.account1.deposit(0)
+
+    def test_transfer_success(self):
+        self.account1.transfer(self.account2, 50)
+        self.assertEqual(self.account1.balance, 50)
+        self.assertEqual(self.account2.balance, 75)
+
+    def test_transfer_account_doesnt_exist(self):
+        with self.assertRaises(ValueError):
+            self.account1.transfer(None, 50)
 
 if __name__ == '__main__':
     unittest.main()
